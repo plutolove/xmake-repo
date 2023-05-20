0311fd813dae4c67d98e1daa0936e6115ab75d20
Shangqi Meng <meng-shangqi@qq.com>
2023-05-20 21:41:20 +0800

    Update xmake.lua

--- packages/a/abseil/xmake.lua
+++ packages/a/abseil/xmake.lua
@@ -16,171 +16,27 @@ package("abseil")
     add_deps("cmake")
 
     add_links(
-        "absl_log_severity",
-
-"absl_raw_logging_internal",
-
-"absl_spinlock_wait",
-
-"absl_malloc_internal",
-
-"absl_base",
-
-"absl_throw_delegate",
-
-"absl_scoped_set_env",
-
-"absl_strerror",
-
-"absl_hashtablez_sampler",
-
-"absl_raw_hash_set",
-
-"absl_crc_cpu_detect",
-
-"absl_crc_internal",
-
-"absl_crc32c",
-
-"absl_crc_cord_state",
-
-"absl_stacktrace",
-
-"absl_symbolize",
-
-"absl_examine_stack",
-
-"absl_failure_signal_handler",
-
-"absl_debugging_internal",
-
-"absl_demangle_internal",
-
-"absl_leak_check",
-
-"absl_flags_program_name",
-
-"absl_flags_config",
-
-"absl_flags_marshalling",
-
-"absl_flags_commandlineflag_internal",
-
-"absl_flags_commandlineflag",
-
-"absl_flags_private_handle_accessor",
-
-"absl_flags_reflection",
-
-"absl_flags_internal",
-
-"absl_flags",
-
-"absl_flags_usage_internal",
-
-"absl_flags_usage",
-
-"absl_flags_parse",
-
-"absl_hash",
-
-"absl_city",
-
-"absl_low_level_hash",
-
-"absl_log_internal_check_op",
-
-"absl_log_internal_conditions",
-
-"absl_log_internal_format",
-
-"absl_log_internal_globals",
-
-"absl_log_internal_proto",
-
-"absl_log_internal_message",
-
-"absl_log_internal_log_sink_set",
-
-"absl_log_internal_nullguard",
-
-"absl_die_if_null",
-
-"absl_log_flags",
-
-"absl_log_globals",
-
-"absl_log_initialize",
-
-"absl_log_entry",
-
-"absl_log_sink",
-
-"absl_int128",
-
-"absl_exponential_biased",
-
-"absl_periodic_sampler",
-
-"absl_random_distributions",
-
-"absl_random_seed_gen_exception",
-
-"absl_random_seed_sequences",
-
-"absl_random_internal_seed_material",
-
-"absl_random_internal_pool_urbg",
-
-"absl_random_internal_platform",
-
-"absl_random_internal_randen",
-
-"absl_random_internal_randen_slow",
-
-"absl_random_internal_randen_hwaes",
-
-"absl_random_internal_randen_hwaes_impl",
-
-"absl_random_internal_distribution_test_util",
-
-"absl_status",
-
-"absl_statusor",
-
-"absl_strings",
-
-"absl_strings_internal",
-
-"absl_str_format_internal",
-
-"absl_cord_internal",
-
-"absl_cordz_functions",
-
-"absl_cordz_handle",
-
-"absl_cordz_info",
-
-"absl_cordz_sample_token",
-
-"absl_cord",
-
-"absl_graphcycles_internal",
-
-"absl_synchronization",
-
-"absl_time",
-
-"absl_civil_time",
-
-"absl_time_zone",
-
-"absl_bad_any_cast_impl",
-
-"absl_bad_optional_access",
-
-"absl_bad_variant_access")
+        "absl_status", "absl_cord",
+        "absl_flags", "absl_flags_parse", "absl_flags_internal", "absl_flags_reflection", "absl_flags_marshalling",
+        "absl_flags_commandlineflag_internal", "absl_synchronization", "absl_time", "absl_hash", "absl_city", "absl_time_zone",
+        "absl_spinlock_wait", "absl_failure_signal_handler", "absl_bad_optional_access", "absl_flags_commandlineflag",
+        "absl_random_internal_pool_urbg",
+        "absl_cordz_info", "absl_cord_internal", "absl_cordz_functions", "absl_cordz_handle", "absl_cordz_sample_token",
+        "absl_base", "absl_bad_any_cast_impl", "absl_periodic_sampler", "absl_random_distributions",
+        "absl_flags_usage_internal", "absl_random_seed_sequences",
+        "absl_throw_delegate", "absl_stacktrace", "absl_symbolize", "absl_debugging_internal",
+        "absl_flags_private_handle_accessor",
+        "absl_strings", "absl_flags_config", "absl_malloc_internal", "absl_str_format_internal",
+        "absl_flags_usage", "absl_strings_internal", "absl_flags_program_name", "absl_int128",
+        "absl_scoped_set_env", "absl_raw_hash_set", "absl_random_internal_seed_material",
+        "absl_random_internal_randen", "absl_random_internal_randen_slow", "absl_random_internal_randen_hwaes_impl",
+        "absl_random_internal_randen_hwaes",
+        "absl_graphcycles_internal", "absl_exponential_biased", "absl_bad_variant_access", "absl_statusor",
+        "absl_random_internal_distribution_test_util", "absl_random_internal_platform",
+        "absl_hashtablez_sampler", "absl_demangle_internal", "absl_leak_check", "absl_log_internal_format", "absl_log_internal_log_sink_set", "absl_log_internal_message", "absl_log_severity",
+        "absl_log_sink", "absl_raw_logging_internal",
+        "absl_strerror", "absl_examine_stack", "absl_low_level_hash", "absl_random_seed_gen_exception", "absl_civil_time",
+        "absl_crc_cord_state", "absl_crc32c", "absl_crc_cpu_detect", "absl_crc_internal")
 
     if is_plat("macosx") then
         add_frameworks("CoreFoundation")
