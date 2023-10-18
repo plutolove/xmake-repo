288b5127dcda7a9c5ccb29c2b7d293b066fe30e4
Shangqi Meng <meng-shangqi@qq.com>
2023-10-18 20:08:43 +0800

    Update xmake.lua

--- packages/l/libllvm/xmake.lua
+++ packages/l/libllvm/xmake.lua
@@ -87,7 +87,7 @@ package("libllvm")
             "-DLLVM_INSTALL_UTILS=ON",
             "-DLLVM_ENABLE_EH=ON",
             "-DLLVM_ENABLE_RTTI=ON",
-            "-DLLVM_TARGETS_TO_BUILD=all",
+            "-DLLVM_TARGETS_TO_BUILD=X86;NVPTX;AMDGPU",
             "-DMLIR_ENABLE_CUDA_RUNNER=ON"
         }
         os.cd("llvm")
