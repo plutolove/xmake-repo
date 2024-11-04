f45a4549862b05c7779cb15e371d60acd703d523
Shangqi Meng <meng-shangqi@qq.com>
2024-11-04 13:52:33 +0800

    Update xmake.lua

--- packages/l/libpg_query/xmake.lua
+++ packages/l/libpg_query/xmake.lua
@@ -4,7 +4,6 @@ package("libpg_query")
     set_urls("https://github.com/plutolove/libpg_query/archive/refs/tags/$(version).tar.gz",
                  "https://github.com/plutolove/libpg_query.git")
 
-    add_versions("12-0", "d91f7e74fb860d1590e9502d7baa83439d13600b9cc8a93d6b1318b359f18830")
     add_versions("12.1.0", "6d020f6eb22b5390dc9eddfd68382232249cf4cdf9446b4b4261019cd801def4")
     on_install("macosx", "linux", function (package)
         local configs = {
