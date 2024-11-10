05d0746db5d71a85c37fd7a8ba8a849fd8dfe443
Shangqi Meng <meng-shangqi@qq.com>
2024-11-10 11:02:35 +0800

    Update xmake.lua

--- packages/l/libpg_query/xmake.lua
+++ packages/l/libpg_query/xmake.lua
@@ -5,7 +5,7 @@ package("libpg_query")
                  "https://github.com/plutolove/libpg_query.git")
 
     add_versions("12.1.0", "6d020f6eb22b5390dc9eddfd68382232249cf4cdf9446b4b4261019cd801def4")
-    add_versions("duckdb_parser-1.1.3", "11c5228c26e9d2554d19e4925b544c4ec47e7e871b39f754a933f13adc90ca9b")
+    add_versions("duckdb_parser-1.1.3", "06fc8b1fc2ee0869a5c6eea73ee717e6ae84e26efeaab15af3e22f3ce6817664")
     on_install("macosx", "linux", function (package)
         local configs = {
             "-DCMAKE_BUILD_TYPE=Release"
